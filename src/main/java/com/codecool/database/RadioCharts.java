306854739a3f6ee25db5350bc2f49db30de851ee
Gróf András <andrasgrof93@gmail.com>
2021-03-24 16:28:41 +0100

    almost done!
    2 test for artist draw

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,49 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+
 public class RadioCharts {
-    //TODO: implement this class
+    private final String DB_URL;
+    private final String DB_USER;
+    private final String DB_PASSWORD;
+
+    public RadioCharts(String db_url, String db_user, String db_password) {
+        DB_URL = db_url;
+        DB_USER = db_user;
+        DB_PASSWORD = db_password;
+    }
+
+    public String getMostPlayedSong() {
+        String result = "";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String sql = "SELECT song FROM music_broadcast ORDER BY times_aired DESC LIMIT 1";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                result = rs.getString("song");
+            }
+            rs.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getMostActiveArtist() {
+        String result = "";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String sql = "SELECT artist,COUNT(artist) AS occurence FROM music_broadcast GROUP BY artist ORDER BY occurence DESC LIMIT 1";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                result = rs.getString("artist");
+            }
+            rs.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
+    }
 }
+
